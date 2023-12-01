4d97cddc8f43929bf4724f85310ec988e5263e40
20120472 <hienthai.030102@gmail.com>
2023-12-02 00:34:41 +0700

    created a page for password reseting

---
+++ src/page/ResetPassword/ResetPasswordPage.jsx
@@ -0,0 +1,152 @@
+import { ErrorMessage } from "@hookform/error-message";
+import { Button, Input } from "@material-tailwind/react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useLocation, useNavigate } from "react-router";
+import { FaCheck } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import Countdown from "react-countdown";
+
+function ResetPasswordPage({ title }) {
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
+  const { search } = useLocation();
+  const navigate = useNavigate();
+
+  function onSubmit(data) {
+    console.log(data);
+    const reqData = {
+      token: new URLSearchParams(search).get("token_id") || "",
+      password: data.password,
+    };
+
+    // Call api with data = reqData. If error happens, use setError(error message);
+    setSuccess(true);
+  }
+
+  function handleCountComplete() {
+    navigate("/sign-in");
+  }
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      password: "",
+      confirm: "",
+    },
+    mode: "onSubmit",
+    reValidateMode: "onBlur",
+  });
+  return (
+    <>
+      {!success && (
+        <div className="min-h-screen bg-indigo-50 py-20">
+          <div className="w-4/12 bg-white shadow-sm rounded-xl p-12 mx-auto">
+            <h1 className=" text-blue-gray-900 font-extrabold text-3xl mt-5 mb-9">
+              {title || "Reset password"}
+            </h1>
+            {error && (
+              <h6 className="text-red-600 text-sm italic mb-5">{error}</h6>
+            )}
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex flex-col gap-8">
+                <div>
+                  <Input
+                    {...register("password", {
+                      required: {
+                        value: true,
+                        message: "Password is required!",
+                      },
+                      minLength: {
+                        value: 8,
+                        message: "Password must have at least 8 characters!",
+                      },
+                    })}
+                    variant="standard"
+                    label="Password"
+                    type="password"
+                  ></Input>
+                  <ErrorMessage
+                    errors={errors}
+                    name="password"
+                    render={({ message }) => (
+                      <small className="text-red-600 italic mb-5">
+                        {message}
+                      </small>
+                    )}
+                  />
+                </div>
+                <div>
+                  <Input
+                    {...register("confirm", {
+                      required: {
+                        value: true,
+                        message: "Password confirmation is required",
+                      },
+                      validate: (value) => {
+                        if (watch("password") != value)
+                          return "Your password do not match";
+                      },
+                    })}
+                    variant="standard"
+                    label="Confirm password"
+                    type="password"
+                  ></Input>
+                  <ErrorMessage
+                    errors={errors}
+                    name="confirm"
+                    render={({ message }) => (
+                      <small className="text-red-600 italic mb-5">
+                        {message}
+                      </small>
+                    )}
+                  />
+                </div>
+                <Button
+                  className="mt-5 w-full text-center p-3 bg-blue-400 text-sm rounded-md font-semibold normal-case"
+                  type="submit"
+                >
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+      {success && (
+        <div className="bg-indigo-50 flex flex-col items-center pt-20 min-h-screen w-full">
+          <div className="-mt-6 p-6 rounded-full bg-green-200 w-fit">
+            <FaCheck size="2.5rem" className="fill-green-600" />
+          </div>
+          <h1 className="font-bold text-3xl my-3">
+            Password changed successfully
+          </h1>
+          <h6 className="mt-2 text-sm my-1">
+            You will be redirect to&nbsp;
+            <Link to="/sign-in">
+              <span className="text-sm font-semibold text-deep-purple-900 hover:text-deep-purple-400">
+                Sign in
+              </span>
+            </Link>
+            &nbsp;after&nbsp;
+            <Countdown
+              date={Date.now() + 5000}
+              renderer={(props) => (
+                <span className="font-bold">{props.seconds}</span>
+              )}
+              onComplete={handleCountComplete}
+              // autoStart={false}
+            ></Countdown>
+            s
+          </h6>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default ResetPasswordPage;
