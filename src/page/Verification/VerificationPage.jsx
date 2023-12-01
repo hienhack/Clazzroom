f3cb3689c7758556e0672a9195d3d38b1b98060a
20120472 <hienthai.030102@gmail.com>
2023-12-01 11:04:25 +0700

    add verrified state

--- src/page/Verification/VerificationPage.jsx
+++ src/page/Verification/VerificationPage.jsx
@@ -16,9 +16,12 @@ function VerificationPage() {
       <div className="-mt-6 p-6 rounded-full bg-green-200 w-fit">
         <MdEmail size="2rem" className="fill-green-600" />
       </div>
-      {!isVerifying ? (
-        <>
-          <h1 className="font-bold text-3xl my-3">Verify your email address</h1>
+      <h1 className="font-bold text-3xl my-3">
+        {user.is_verified
+          ? "Your email has been verified"
+          : "Verify your email address"}
+      </h1>
+      {/* <>
           <h6 className="text-sm my-1">
             You have to verify your email to continue using this application
           </h6>
@@ -29,8 +32,8 @@ function VerificationPage() {
           <Button className="bg-green-600 mt-8" onClick={handleVerfify}>
             verify
           </Button>
-        </>
-      ) : (
+        </> */}
+      {!user.is_verified && (
         <>
           <h6 className="mt-2 text-sm my-1">
             We have sent a verification link to <b>{user?.email}</b>
