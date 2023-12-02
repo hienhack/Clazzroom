42a1715b7a973c54b3bdd2bd06c51360031e7fba
20120472 <hienthai.030102@gmail.com>
2023-12-02 17:35:40 +0700

    basically finished verification

--- src/page/Verification/VerificationPage.jsx
+++ src/page/Verification/VerificationPage.jsx
@@ -1,40 +1,103 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
-import { MdEmail } from "react-icons/md";
-import { Button } from "@material-tailwind/react";
+import { MdEmail, MdOutlineClose, MdMarkEmailRead } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
+import { Button, Spinner } from "@material-tailwind/react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 function VerificationPage() {
-  const { user, setUser } = useContext(AuthContext);
-  const { isVerifying, setIsVerifying } = useState(false);
+  const { setUser } = useContext(AuthContext);
+  // const { user } = useContext(AuthContext);
 
-  function handleVerfify() {
-    setIsVerifying(true);
+  // test user
+  const user = {
+    is_verified: false,
+    image: "",
+    full_name: "Hien Thai",
+    email: "hienthai@gmail.com",
+  };
+
+  const { search } = useLocation();
+  const token = new URLSearchParams(search).get("token_id");
+  const [error, setError] = useState();
+  const [success, setSuccess] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [sending, setSending] = useState(false);
+
+  useEffect(() => {
+    if (user.is_verified) {
+      setIsProcessing(false);
+      // setIsVerified(true);
+      return;
+    } else if (token == null) {
+      return;
+    }
+
+    axios
+      .post("/users/verify", {
+        token: token,
+      })
+      .then((res) => {
+        setUser({
+          ...user,
+          is_verified: true,
+        });
+        setSuccess(true);
+      })
+      .catch((error) => {
+        setError("This link is expired or invalid");
+        setSuccess(true);
+      });
+  }, []);
+
+  function handleResendBtnClick() {
+    setSending(true);
+    axios
+      .post("/users/resend-verification", {})
+      .then(() => {
+        setSending(false);
+      })
+      .catch((error) => {});
   }
 
   return (
     <div className="bg-indigo-50 flex flex-col items-center pt-20 min-h-screen w-full">
-      <div className="-mt-6 p-6 rounded-full bg-green-200 w-fit">
-        <MdEmail size="2rem" className="fill-green-600" />
-      </div>
-      <h1 className="font-bold text-3xl my-3">
-        {user.is_verified
-          ? "Your email has been verified"
-          : "Verify your email address"}
-      </h1>
-      {/* <>
-          <h6 className="text-sm my-1">
-            You have to verify your email to continue using this application
-          </h6>
-          <h6 className="text-sm my-1">
-            Click the VERIFY button below to get the verification mail sent to
-            your email
-          </h6>
-          <Button className="bg-green-600 mt-8" onClick={handleVerfify}>
-            verify
-          </Button>
-        </> */}
-      {!user.is_verified && (
+      {isProcessing && (
+        <div className="flex items-center gap-3 opacity-80">
+          <Spinner className="h-6 text-blue-gray-700" />
+          <h1 className="text-blue-gray-700 text-xl">Processing...</h1>
+        </div>
+      )}
+
+      {error && (
         <>
+          <div className="-mt-4 p-4 rounded-full bg-red-200 w-fit">
+            <IoClose size="3rem" className="fill-red-600" />
+          </div>
+          <h1 className="font-bold text-3xl my-3">{error}</h1>
+        </>
+      )}
+
+      {((!token && user?.is_verified) || success) && (
+        <>
+          <div className="-mt-6 p-6 rounded-full bg-green-200 w-fit">
+            <MdMarkEmailRead size="2rem" className="fill-green-600" />
+          </div>
+          <h1 className="font-bold text-3xl my-3">
+            Your email has been verified
+          </h1>
+          <h6 className="mt-2 my-1">You can close this page now!</h6>
+        </>
+      )}
+
+      {!token && !user?.is_verified && (
+        <>
+          <div className="-mt-6 p-6 rounded-full bg-green-200 w-fit">
+            <MdEmail size="2rem" className="fill-green-600" />
+          </div>
+          <h1 className="font-bold text-3xl my-3">Verify your email address</h1>
+
           <h6 className="mt-2 text-sm my-1">
             We have sent a verification link to <b>{user?.email}</b>
           </h6>
@@ -44,7 +107,21 @@ function VerificationPage() {
           <h6 className="text-sm my-1">
             You may need to check your spam folder
           </h6>
-          <Button className="bg-green-600 mt-8">Resend email</Button>
+          <h6 className="text-sm my-1">
+            <b>Resend</b> if you couldn't see the mail or it is expired
+          </h6>
+          {sending ? (
+            <Button className="bg-green-600 mt-8 normal-case" disabled>
+              Sending...
+            </Button>
+          ) : (
+            <Button
+              className="bg-green-600 mt-8 normal-case"
+              onClick={handleResendBtnClick}
+            >
+              Resend
+            </Button>
+          )}
         </>
       )}
     </div>
