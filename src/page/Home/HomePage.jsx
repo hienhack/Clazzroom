47efca0553c9733429ef8bc3fb0b2a36949d0a81
20120472 <hienthai.030102@gmail.com>
2023-12-04 00:14:41 +0700

    reconfig

--- src/page/Home/HomePage.jsx
+++ src/page/Home/HomePage.jsx
@@ -1,9 +1,5 @@
 function HomePage() {
-  return (
-    <div className="h-[1000px] bg-indigo-300">
-      <h1>This is the homepage</h1>
-    </div>
-  );
+  return <div className="h-[1000px] bg-indigo-50"></div>;
 }
 
 export default HomePage;
