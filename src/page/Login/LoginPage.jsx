cce7a7e47d9ba124881f77ca4ac442343b5e1031
thanhlaunchdeck <thanh.le@launchdeck.org>
2023-12-04 20:29:57 +0700

    update

--- src/page/Login/LoginPage.jsx
+++ src/page/Login/LoginPage.jsx
@@ -36,7 +36,7 @@ function LoginPage() {
     axios
       .post(url, data)
       .then((res) => {
-        loginToContext(res.data);
+        loginToContext(res.data.data);
         navigate("/");
       })
       .catch((error) => {
