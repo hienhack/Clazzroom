cbec83a4b7b5a1d59dda6559c37a04105bc0e617
thanhlaunchdeck <thanh.le@launchdeck.org>
2023-12-07 18:07:32 +0700

    update google login

--- src/page/Login/LoginPage.jsx
+++ src/page/Login/LoginPage.jsx
@@ -142,127 +142,127 @@ function LoginPage() {
 
   const { loginWithPopup, user } = useAuth0();
 
-  const handleLoginWithGoogle = () => {
-    const handleLoginWithGoogle = async () => {
-      try {
-        await loginWithPopup({
-          connection: 'google-oauth2',
-          scope: 'openid email profile' // Phạm vi yêu cầu từ Google
-        });
-        console.log(user);
-      } catch (error) {
-        console.error('Login failed:', error);
-      }
-    };
 
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm({
-      defaultValues: {
-        email: "",
-        password: "",
-      },
-      mode: "onSubmit",
-      reValidateMode: "onBlur",
-    });
+  const handleLoginWithGoogle = async () => {
+    try {
+      await loginWithPopup({
+        connection: 'google-oauth2',
+        scope: 'openid email profile' // Phạm vi yêu cầu từ Google
+      });
+      console.log(user);
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
 
-    return (
-      <>
-        <div className="min-h-screen bg-indigo-50 py-20">
-          <div className="w-4/12 bg-white shadow-sm rounded-xl p-12 mx-auto">
-            {!registerNeeded && !forgotPw && (
-              <>
-                <h1 className="text-center text-blue-gray-900 font-extrabold text-3xl mt-5 mb-9">
-                  Sign in
-                </h1>
-                {error && (
-                  <h6 className="text-red-600 text-sm italic mb-5">{error}</h6>
-                )}
-                <form onSubmit={handleSubmit(onSubmit)}>
-                  <div className="flex flex-col gap-8">
-                    <div>
-                      <Input
-                        {...register("email", {
-                          required: {
-                            value: true,
-                            message: "Email is required!",
-                          },
-                          pattern: {
-                            value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-                            message: "Invalid email",
-                          },
-                        })}
-                        variant="standard"
-                        label="Email"
-                      ></Input>
-                      <ErrorMessage
-                        errors={errors}
-                        name="email"
-                        render={({ message }) => (
-                          <small className="text-red-600 italic mb-5">
-                            {message}
-                          </small>
-                        )}
-                      />
-                    </div>
-                    <div>
-                      <Input
-                        {...register("password", {
-                          required: {
-                            value: true,
-                            message: "Password is required!",
-                          },
-                          minLength: {
-                            value: 8,
-                            message: "Password must have at least 8 characters!",
-                          },
-                        })}
-                        variant="standard"
-                        label="Password"
-                        type="password"
-                      ></Input>
-                      <ErrorMessage
-                        errors={errors}
-                        name="password"
-                        render={({ message }) => (
-                          <small className="text-red-600 italic mb-5">
-                            {message}
-                          </small>
-                        )}
-                      />
-                      <div>
-                        <button
-                          type="button"
-                          className="mt-2 block float-right text-xs text-blue-gray-400 hover:text-blue-gray-700"
-                          onClick={() => setForgotPw(true)}
-                        >
-                          Forgot your password?
-                        </button>
-                      </div>
-                    </div>
-                    <Button
-                      className="w-full text-center p-3 bg-blue-400 text-sm rounded-md font-semibold normal-case"
-                      type="submit"
-                      disabled={sending}
-                    >
-                      {sending ? "Processing..." : "Continue"}
-                    </Button>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onSubmit",
+    reValidateMode: "onBlur",
+  });
+
+  return (
+    <>
+      <div className="min-h-screen bg-indigo-50 py-20">
+        <div className="w-4/12 bg-white shadow-sm rounded-xl p-12 mx-auto">
+          {!registerNeeded && !forgotPw && (
+            <>
+              <h1 className="text-center text-blue-gray-900 font-extrabold text-3xl mt-5 mb-9">
+                Sign in
+              </h1>
+              {error && (
+                <h6 className="text-red-600 text-sm italic mb-5">{error}</h6>
+              )}
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-8">
+                  <div>
+                    <Input
+                      {...register("email", {
+                        required: {
+                          value: true,
+                          message: "Email is required!",
+                        },
+                        pattern: {
+                          value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                          message: "Invalid email",
+                        },
+                      })}
+                      variant="standard"
+                      label="Email"
+                    ></Input>
+                    <ErrorMessage
+                      errors={errors}
+                      name="email"
+                      render={({ message }) => (
+                        <small className="text-red-600 italic mb-5">
+                          {message}
+                        </small>
+                      )}
+                    />
                   </div>
-                </form>
-                <div className="my-5 flex items-center gap-2">
-                  <hr className="grow border-blue-gray-200" />
-                  <h6 className="text-gray-700 text-sm">Or continue with</h6>
-                  <hr className="grow border-blue-gray-200" />
+                  <div>
+                    <Input
+                      {...register("password", {
+                        required: {
+                          value: true,
+                          message: "Password is required!",
+                        },
+                        minLength: {
+                          value: 8,
+                          message: "Password must have at least 8 characters!",
+                        },
+                      })}
+                      variant="standard"
+                      label="Password"
+                      type="password"
+                    ></Input>
+                    <ErrorMessage
+                      errors={errors}
+                      name="password"
+                      render={({ message }) => (
+                        <small className="text-red-600 italic mb-5">
+                          {message}
+                        </small>
+                      )}
+                    />
+                    <div>
+                      <button
+                        type="button"
+                        className="mt-2 block float-right text-xs text-blue-gray-400 hover:text-blue-gray-700"
+                        onClick={() => setForgotPw(true)}
+                      >
+                        Forgot your password?
+                      </button>
+                    </div>
+                  </div>
+                  <Button
+                    className="w-full text-center p-3 bg-blue-400 text-sm rounded-md font-semibold normal-case"
+                    type="submit"
+                    disabled={sending}
+                  >
+                    {sending ? "Processing..." : "Continue"}
+                  </Button>
                 </div>
-                <div className="grid grid-cols-2  gap-3">
-                  <button className="flex justify-center items-center gap-2 w-full p-[0.6rem] fill-white text-white rounded-md bg-red-700 hover:bg-red-800" onClick={handleLoginWithGoogle}>
-                    <FaGoogle size="1.2rem" className="fill-inherit" />
-                    <span>Google</span>
-                  </button>
+              </form>
+              <div className="my-5 flex items-center gap-2">
+                <hr className="grow border-blue-gray-200" />
+                <h6 className="text-gray-700 text-sm">Or continue with</h6>
+                <hr className="grow border-blue-gray-200" />
+              </div>
+              <div className="grid grid-cols-2  gap-3">
+                <button className="flex justify-center items-center gap-2 w-full p-[0.6rem] fill-white text-white rounded-md bg-red-700 hover:bg-red-800" onClick={handleLoginWithGoogle}>
+                  <FaGoogle size="1.2rem" className="fill-inherit" />
+                  <span>Google</span>
+                </button>
 
-                  {/* <LoginSocialGoogle
+                {/* <LoginSocialGoogle
                   client_id="808993990616-cp2jebgeusd5vdcq1nikroc95etecuim.apps.googleusercontent.com"
                   discoveryDocs="claims_supported"
                   access_type="offline"
@@ -273,25 +273,25 @@ function LoginPage() {
                 >
                   
                 </LoginSocialGoogle> */}
-                  <FacebookLogin
-                    appId="2580168245493289"
-                    onSuccess={(response) => { }}
-                    onFail={(error) => {
-                      console.log("Login Failed!", error);
-                    }}
-                    onProfileSuccess={(response) => {
-                      handleFacebookLogin(response);
-                    }}
+                <FacebookLogin
+                  appId="2580168245493289"
+                  onSuccess={(response) => { }}
+                  onFail={(error) => {
+                    console.log("Login Failed!", error);
+                  }}
+                  onProfileSuccess={(response) => {
+                    handleFacebookLogin(response);
+                  }}
 
-                  // )}
-                  >
-                    <button className="flex justify-center items-center gap-2 w-full p-[0.6rem] fill-white text-white rounded-md bg-blue-800 hover:bg-blue-900">
-                      <FaFacebook size="1.2rem" className="fill-inherit" />
-                      <span>Facebook</span>
-                    </button>
-                  </FacebookLogin>
+                // )}
+                >
+                  <button className="flex justify-center items-center gap-2 w-full p-[0.6rem] fill-white text-white rounded-md bg-blue-800 hover:bg-blue-900">
+                    <FaFacebook size="1.2rem" className="fill-inherit" />
+                    <span>Facebook</span>
+                  </button>
+                </FacebookLogin>
 
-                  {/* <LoginSocialFacebook
+                {/* <LoginSocialFacebook
                   appId="2580168245493289"
                   fields="name,email,picture"
                   onResolve={(response) => {
@@ -305,40 +305,40 @@ function LoginPage() {
                     <span>Facebook</span>
                   </button>
                 </LoginSocialFacebook> */}
-                </div>
-                <div className="mt-10 flex gap-2 items-center">
-                  <h1 className="text-sm text-gray-700">
-                    Did not have an account yet?
-                  </h1>
-                  <Link to="/sign-up">
-                    <h6
-                      className="font-semibold text-sm text-deep-purple-900 hover:text-deep-purple-400"
-                      href="#"
-                    >
-                      Create now
-                    </h6>
-                  </Link>
-                </div>{" "}
-              </>
-            )}
-            {registerNeeded && (
-              <MoreInfoForm
-                login={login}
-                handleCancel={handleCancelRegistor}
-                user={loadedUser}
-              />
-            )}
-            {forgotPw && (
-              <ForgotPasswordForm
-                onBack={() => {
-                  setForgotPw(false);
-                }}
-              />
-            )}
-          </div>
+              </div>
+              <div className="mt-10 flex gap-2 items-center">
+                <h1 className="text-sm text-gray-700">
+                  Did not have an account yet?
+                </h1>
+                <Link to="/sign-up">
+                  <h6
+                    className="font-semibold text-sm text-deep-purple-900 hover:text-deep-purple-400"
+                    href="#"
+                  >
+                    Create now
+                  </h6>
+                </Link>
+              </div>{" "}
+            </>
+          )}
+          {registerNeeded && (
+            <MoreInfoForm
+              login={login}
+              handleCancel={handleCancelRegistor}
+              user={loadedUser}
+            />
+          )}
+          {forgotPw && (
+            <ForgotPasswordForm
+              onBack={() => {
+                setForgotPw(false);
+              }}
+            />
+          )}
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
 
-  export default LoginPage;
+export default LoginPage;
