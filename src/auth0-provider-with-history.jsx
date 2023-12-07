480a81588e0e0f97130d5941bab389ed2e6031ce
thanhlaunchdeck <thanh.le@launchdeck.org>
2023-12-07 17:23:44 +0700

    update auth0

--- src/auth0-provider-with-history.jsx
+++ src/auth0-provider-with-history.jsx
@@ -3,13 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const navigate = useNavigate();
   const domain = "dev-3cbpv7st47pr6uhr.us.auth0.com";
   const clientId = "1Ozu9uQIsahvKt7ioJHf8k2CRfQ7iGfC";
 
-  const onRedirectCallback = (appState) => {
-    navigate(appState?.returnTo || window.location.pathname);
-  }
+
 
 
   return (
@@ -17,7 +14,6 @@ const Auth0ProviderWithHistory = ({ children }) => {
       domain={domain}
       clientId={clientId}
       redirectUri={window.location.origin}
-      onRedirectCallback={onRedirectCallback}
     >
       {children}
     </Auth0Provider>
