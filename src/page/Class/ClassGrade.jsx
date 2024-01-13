7ebb8ab24512e8289a9956ea412a5ae944ae21a2
20120472 <hienthai.030102@gmail.com>
2024-01-13 16:38:38 +0700

    fixed student grade

--- src/page/Class/ClassGrade.jsx
+++ src/page/Class/ClassGrade.jsx
@@ -9,111 +9,227 @@ import {
   Tooltip,
   Typography,
 } from "@material-tailwind/react";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useContext, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { MdOutlineQuestionAnswer } from "react-icons/md";
 import InputFormat from "../../component/common/InputFormat";
 import { getStandardText } from "../../utils/InputFormatHelper";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+function total(grades) {
+  let result = 0;
+  grades.forEach((g) => {
+    if (g.value != null && !isNaN(g.value)) {
+      result = result + (g.value * g.scale) / 100;
+    }
+  });
+
+  return result.toFixed(2);
+}
 
 function ClassGrade() {
   const [showReviewDialog, setShowReviewDialog] = useState(false);
-  const [currentGrade, setCurrentGrade] = useState(0);
-  const [composition, setComposition] = useState(" ");
+  const [loading, setLoading] = useState(true);
+  const [gradeBoard, setGradeBoard] = useState(null);
+  const [composition, setComposition] = useState(null);
+  const { classId } = useParams();
+  const { user } = useContext(AuthContext);
 
-  function handleReviewClick(name, currentGrade) {
-    setComposition(name);
-    setCurrentGrade(currentGrade);
+  function handleReviewClick(index) {
+    setComposition(gradeBoard[index]);
     setShowReviewDialog(true);
   }
+
+  function handleReviewSuccess() {
+    const index = gradeBoard.findIndex((r) => r._id == composition._id);
+    if (index == -1) return;
+    const newGradeBoard = gradeBoard.slice();
+    newGradeBoard[index].state = "is_reviewed";
+    setGradeBoard(newGradeBoard);
+  }
+
+  useEffect(() => {
+    if (user && !user.student_id) return;
+    axios
+      .get(`/classes/${classId}/grade-board`)
+      .then((res) => {
+        setGradeBoard(res.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        alert("Something went wrong, please reload the page!");
+      });
+  }, []);
 
   return (
     <>
       <div className="p-6 h-full overflow-y-auto">
-        <div className="w-[600px] bg-white mx-auto drop-shadow-md rounded-lg">
-          <div className="grid grid-cols-10 bg-indigo-800 text-white rounded-t-lg font-medium">
-            <div className="py-4">
-              <h6 className="text-sm text-center leading-6">#</h6>
-            </div>
-            <div className="col-span-4 py-4">
-              <span className="text-sm">Composition name</span>
-            </div>
-            <div className="col-span-2 py-4 text-center">
-              <span className="text-sm">Scale (%)</span>
-            </div>
-            <div className="col-span-2 py-4 text-center">
-              <span className="text-sm">Point</span>
-            </div>
-            <div className="py-4"></div>
+        {user?.student_id == null && (
+          <div className="mt-20 flex flex-col items-center">
+            <h6 className="text-blue-gray-800">
+              You have to map your student ID to be able to see your grades
+            </h6>
+            <Link to="/account">
+              <span className="text-indigo-600 text-sm hover:underline hover:text-indigo-800 font-medium">
+                Go to my account
+              </span>
+            </Link>
           </div>
-          <div className="grid grid-cols-10">
-            {/*  */}
-            <hr className="border-gray-300 col-span-10" />
-            <div className="py-4 text-center">
-              <span className="text-sm">1</span>
+        )}
+        {user?.student_id != null && !loading && (
+          <div className="w-[600px] bg-white mx-auto drop-shadow-md rounded-lg">
+            <div className="grid grid-cols-10 bg-indigo-800 text-white rounded-t-lg font-medium">
+              <div className="py-4">
+                <h6 className="text-sm text-center leading-6">#</h6>
+              </div>
+              <div className="col-span-4 py-4">
+                <span className="text-sm">Composition name</span>
+              </div>
+              <div className="col-span-2 py-4 text-center">
+                <span className="text-sm">Scale (%)</span>
+              </div>
+              <div className="col-span-2 py-4 text-center">
+                <span className="text-sm">Point</span>
+              </div>
+              <div className="py-4"></div>
             </div>
-            <div className="col-span-4 py-4">
-              <span className="text-sm">Assignments</span>
-            </div>
-            <div className="col-span-2 py-4 text-center">
-              <span className="text-sm">20</span>
-            </div>
-            <div className="col-span-2 py-4 text-center">
-              <span className="text-sm">10</span>
-            </div>
-            <div className="py-4">
-              <div className="flex items-center h-full">
-                <Tooltip
-                  className="bg-gray-700 text-xs py-1"
-                  placement="bottom"
-                  content="Request a review"
-                >
-                  <button className="fill-blue-gray-500 hover:fill-blue-gray-800">
-                    <MdOutlineQuestionAnswer
-                      className="fill-inherit"
-                      size="1.2rem"
-                      onClick={() => handleReviewClick("Assignments", 1)}
-                    />
-                  </button>
-                </Tooltip>
+            <div className="grid grid-cols-10">
+              {gradeBoard?.map((r, index) => (
+                <>
+                  <hr className="border-gray-300 col-span-10" />
+                  <div className="py-4 text-center">
+                    <span className="text-sm">{index + 1}</span>
+                  </div>
+                  <div className="col-span-4 py-4">
+                    <span className="text-sm">{r.name}</span>
+                  </div>
+                  <div className="col-span-2 py-4 text-center">
+                    <span className="text-sm">{r.scale}</span>
+                  </div>
+                  <div className="col-span-2 py-4 text-center">
+                    <span className="text-sm">{r.value}</span>
+                  </div>
+                  <div className="py-4">
+                    {!r.is_reviewed && (
+                      <div className="flex items-center h-full">
+                        <Tooltip
+                          className="bg-gray-700 text-xs py-1"
+                          placement="bottom"
+                          content="Request a review"
+                        >
+                          <button className="fill-blue-gray-500 hover:fill-blue-gray-800">
+                            <MdOutlineQuestionAnswer
+                              className="fill-inherit"
+                              size="1.2rem"
+                              onClick={() => handleReviewClick(index)}
+                            />
+                          </button>
+                        </Tooltip>
+                      </div>
+                    )}
+                  </div>
+                </>
+              ))}
+              <hr className="border-gray-300 col-span-10" />
+              <div className="col-span-7 text-center py-4">
+                <span className="text-sm font-medium">Total</span>
+              </div>
+              <div className="col-span-2 text-center py-4">
+                <span className="text-sm font-medium">
+                  {gradeBoard ? total(gradeBoard) : 0}
+                </span>
               </div>
             </div>
-            {/*  */}
-            <hr className="border-gray-300 col-span-10" />
-            <div className="col-span-7 text-center py-4">
-              <span className="text-sm font-medium">Total</span>
+          </div>
+        )}
+        {user?.student_id != null && loading && (
+          <div className="w-[600px] bg-white mx-auto drop-shadow-md rounded-lg">
+            <div className="grid grid-cols-10 bg-indigo-800 text-white rounded-t-lg font-medium">
+              <div className="py-4">
+                <h6 className="text-sm text-center leading-6">#</h6>
+              </div>
+              <div className="col-span-4 py-4">
+                <span className="text-sm">Composition name</span>
+              </div>
+              <div className="col-span-2 py-4 text-center">
+                <span className="text-sm">Scale (%)</span>
+              </div>
+              <div className="col-span-2 py-4 text-center">
+                <span className="text-sm">Point</span>
+              </div>
+              <div className="py-4"></div>
             </div>
-            <div className="col-span-2 text-center py-4">
-              <span className="text-sm font-medium">0</span>
+            <div className="grid grid-cols-10">
+              {[1, 2, 3, 4, 5].map((v) => (
+                <>
+                  <hr className="border-gray-300 col-span-10" />
+                  <div className="py-4">
+                    <div className="w-4/12 h-2 animate-pulse bg-gray-300 mx-auto"></div>
+                  </div>
+                  <div className="col-span-4 py-4">
+                    <div className="w-7/12 h-2 animate-pulse bg-gray-300"></div>
+                  </div>
+                  <div className="col-span-2 py-4 text-center">
+                    <div className="w-4/12 h-2 animate-pulse bg-gray-300 mx-auto"></div>
+                  </div>
+                  <div className="col-span-2 py-4 text-center">
+                    <div className="w-4/12 h-2 animate-pulse bg-gray-300 mx-auto"></div>
+                  </div>
+                  <div className="py-4"></div>
+                </>
+              ))}
+              <hr className="border-gray-300 col-span-10" />
+              <div className="col-span-7 text-center py-4">
+                <span className="text-sm font-medium">Total</span>
+              </div>
+              <div className="col-span-2 text-center py-4">
+                <div className="w-4/12 h-2 animate-pulse bg-gray-300 mx-auto mt-1"></div>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
       <CreateReviewDialog
-        currentGrade={currentGrade}
         composition={composition}
         open={showReviewDialog}
         handleOpen={() => setShowReviewDialog(false)}
+        handleSuccess={handleReviewSuccess}
       />
     </>
   );
 }
 
-function CreateReviewDialog({ composition, currentGrade, open, handleOpen }) {
+function CreateReviewDialog({ composition, handleSuccess, open, handleOpen }) {
   const [processing, setProcessing] = useState(false);
+  const { classId } = useParams();
   const explanationRef = useRef(null);
   const [error, setError] = useState(null);
 
   function onSubmit(data) {
-    // setProcessing(true);
+    setProcessing(true);
     const explanation = getStandardText(explanationRef.current.innerHTML);
-    console.log(explanation);
     if (explanation == "") {
       setError("Explanation is required");
       return;
     }
-
     data.explanation = explanation;
-    console.log(data);
+    data.class_id = classId;
+    data.grade_composition_id = composition._id;
+
+    axios
+      .post("/reviews", data)
+      .then((res) => {
+        toast.success("Review created");
+        handleSuccess();
+        handleCancel();
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      })
+      .finally(() => setProcessing(false));
   }
 
   if (explanationRef.current != null) {
@@ -133,12 +249,8 @@ function CreateReviewDialog({ composition, currentGrade, open, handleOpen }) {
     register,
     handleSubmit,
     reset,
-    formState: { submitCount, errors },
+    formState: { errors },
   } = useForm({
-    defaultValues: {
-      composition: composition,
-      expectation_grade: currentGrade,
-    },
     mode: "onSubmit",
     reValidateMode: "onBlur",
   });
@@ -148,7 +260,7 @@ function CreateReviewDialog({ composition, currentGrade, open, handleOpen }) {
       size="md"
       open={open}
       handler={handleOpen}
-      dismiss={{ enabled: !processing }}
+      dismiss={{ enabled: false }}
       className="bg-transparent shadow-none"
     >
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -159,7 +271,7 @@ function CreateReviewDialog({ composition, currentGrade, open, handleOpen }) {
             </Typography>
             <div className="flex flex-col gap-6">
               <Input
-                value={composition}
+                value={composition?.name}
                 className="mb-4"
                 variant="standard"
                 label="Grade composition"
@@ -167,7 +279,7 @@ function CreateReviewDialog({ composition, currentGrade, open, handleOpen }) {
               ></Input>
               <div className="grid grid-cols-2 gap-6">
                 <Input
-                  value={currentGrade}
+                  value={composition?.value}
                   className="mb-4"
                   variant="standard"
                   label="Current grade"
@@ -187,6 +299,7 @@ function CreateReviewDialog({ composition, currentGrade, open, handleOpen }) {
                         }
                       },
                     })}
+                    defaultValue={composition?.value}
                     className="mb-4"
                     variant="standard"
                     label="Expectation grade"
@@ -235,7 +348,7 @@ function CreateReviewDialog({ composition, currentGrade, open, handleOpen }) {
                 color="blue"
                 disabled={processing}
               >
-                <span>Send</span>
+                <span>{processing ? "Sending..." : "Send"}</span>
               </Button>
             </div>
           </CardFooter>
